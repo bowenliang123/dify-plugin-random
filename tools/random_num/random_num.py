81950cfde97efbe6b35af6d13b02fcb416503c19
Bowen Liang <liangbowen@gf.com.cn>
2025-03-13 01:38:57 +0800

    check range

--- tools/random_num/random_num.py
+++ tools/random_num/random_num.py
@@ -12,13 +12,16 @@ class RandomNumTool(Tool):
         upper_bound = tool_parameters.get("upper_bound", 1024)
         digits = int(tool_parameters.get("digits", 0))
 
-        if float(lower_bound) > float(upper_bound):
+        lower_num = float(lower_bound)
+        upper_num = float(upper_bound)
+        if lower_num > upper_num:
             raise ValueError("Invalid range, the the lower bound should be less than or equal to the upper bound")
 
         if digits < 0:
-            raise ValueError("Invalid digits")
+            raise ValueError(f"Invalid digits {digits}")
         elif digits == 0:
-            random_num = random.randint(int(lower_bound), int(upper_bound))
+            random_num = random.randint(int(lower_num), int(upper_num))
         else:
-            random_num = round(random.uniform(float(lower_bound), float(upper_bound)), digits)
+            random_num = round(random.uniform(lower_num, upper_num), digits)
+
         yield self.create_text_message(str(random_num))
