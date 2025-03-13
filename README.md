5a82aab486f13dd29f9f1d1590bbb06309a48a81
Bowen Liang <liangbowen@gf.com.cn>
2025-03-13 11:19:53 +0800

    introduce random_string tool

--- README.md
+++ README.md
@@ -8,7 +8,12 @@
 
 A Dify plugin to generate random number or string.
 
-- `random_num` Tool: Generate a random number in range. If the digits is set to 0, an integer will be generated.
+
+- `random_string` Tool: Generate a random string with given length from alphabets and numbers.
+
+eg. `TSUWUBoOIu4bZja7mdjTkKteaKVrhz`
+
+- `random_num` Tool: Generate a random number in given range. If the digits is set to 0, an integer will be generated.
 
 <img src="./_assets/img1.png" width="400px" >
 <img src="./_assets/img2.png" width="400px" >
---
+++ tools/random_string/random_string.py
@@ -0,0 +1,19 @@
+import random
+import string
+from collections.abc import Generator
+from typing import Any
+
+from dify_plugin import Tool
+from dify_plugin.entities.tool import ToolInvokeMessage
+
+
+class RandomStringTool(Tool):
+    def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
+        length = int(tool_parameters.get("length", 0))
+        if not length or length < 0:
+            yield self.create_text_message("")
+            return
+
+        characters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(characters) for _ in range(length))
+        yield self.create_text_message(random_string)
