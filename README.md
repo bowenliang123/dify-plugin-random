fe9ffeffe0ef67719fc10d953e0b28ccb979aefb
Bowen Liang <liangbowen@gf.com.cn>
2025-03-18 23:35:01 +0800

    update

--- README.md
+++ README.md
@@ -8,26 +8,33 @@
 
 A Dify plugin to generate random number, string, UUID, prime number and etc.
 
-
 ## Tools
 
 ### Random String:
-  - Generate a random string with given length from alphabets and numbers.
-  - Output: eg. `TSUWUBoOIu4bZja7mdjTkKteaKVrhz` in length 30.
+
+- Generate a random string with given length from alphabets and numbers.
+- Output: eg. `TSUWUBoOIu4bZja7mdjTkKteaKVrhz` in length 30.
 
 ### Random Number:
-  - Generate a random number in given range [ lower_bound, upper bound ] .
-  - Output: If the digits is set to 0, an integer will be generated.
+
+- Generate a random number in given range [ lower_bound, upper bound ] .
+- Parameters:
+    - `include_alphabets`: Include alphabets from both, either or none of uppercase or lowercase alphabets.
+    - `include_numbers`: Whether to include numbers
+    - `include_punctuation`: Whether to include punctuation characters from `!"#$%&'()*+,-./:;<=>?@[\]^_``{|}~`
+- Output: If the digits is set to 0, an integer will be generated.
 
   <img src="./_assets/img1.png" width="400px" >
   <img src="./_assets/img2.png" width="400px" >
 
 ### Random Prime Number:
-  - Generate a random prime number in given range [ lower_bound, upper bound ] .
-  - Output: A prime number, or `NaN` if no prime number is found.
+
+- Generate a random prime number in given range [ lower_bound, upper bound ] .
+- Output: A prime number, or `NaN` if no prime number is found.
 
 ### Random UUID:
-  - Generate a random UUID string of UUID v4.
+
+- Generate a random UUID string of UUID v4.
 
 <img src="./_assets/img3.png" width="400px" >
 
--- tools/random_string/random_string.py
+++ tools/random_string/random_string.py
@@ -14,6 +14,43 @@ class RandomStringTool(Tool):
             yield self.create_text_message("")
             return
 
-        characters = string.ascii_letters + string.digits
+        include_alphabets = tool_parameters.get("include_alphabets", "upper_and_lower")
+        include_numbers = tool_parameters.get("include_numbers", "true").lower()
+        include_punctuation = tool_parameters.get("include_punctuation", "false").lower()
+
+        characters = (self.append_alphabets(include_alphabets)
+                      + self.append_numbers(include_numbers)
+                      + self.append_punctuation(include_punctuation))
+
         random_string = ''.join(random.choice(characters) for _ in range(length))
         yield self.create_text_message(random_string)
+
+    @staticmethod
+    def append_alphabets(include_alphabets: str) -> str:
+        match include_alphabets:
+            case "upper_and_lower":
+                return string.ascii_letters
+            case "uppercase_only":
+                return string.ascii_uppercase
+            case "lowercase_only":
+                return string.ascii_lowercase
+            case "not_included":
+                return ""
+            case _:
+                return ""
+
+    @staticmethod
+    def append_numbers(include_numbers: str) -> str:
+        match include_numbers:
+            case "true":
+                return string.digits
+            case _:
+                return ""
+
+    @staticmethod
+    def append_punctuation(include_punctuation: str) -> str:
+        match include_punctuation:
+            case "true":
+                return string.punctuation
+            case _:
+                return ""
