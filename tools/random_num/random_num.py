6f7cedf154443e291b09458d0459e7e67ba688b4
Bowen Liang <liangbowen@gf.com.cn>
2025-03-19 17:00:15 +0800

    support generate multiple numbers in random_num tool

--- tools/random_num/random_num.py
+++ tools/random_num/random_num.py
@@ -9,8 +9,16 @@ from dify_plugin.entities.tool import ToolInvokeMessage
 class RandomNumTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         lower_bound = tool_parameters.get("lower_bound", 0)
-        upper_bound = tool_parameters.get("upper_bound", 1024)
+        upper_bound = tool_parameters.get("upper_bound", 100)
         digits = int(tool_parameters.get("digits", 0))
+        num_count = int(tool_parameters.get("num_count", 1))
+        separator = tool_parameters.get("separator") or ', '
+
+        if num_count == 0:
+            yield self.create_text_message("")
+            return
+        elif num_count < 0:
+            raise ValueError(f"Invalid num_count {num_count}")
 
         if not lower_bound:
             raise ValueError("Invalid input lower_bound")
@@ -25,9 +33,18 @@ class RandomNumTool(Tool):
 
         if digits < 0:
             raise ValueError(f"Invalid digits {digits}")
-        elif digits == 0:
+
+        # Generate random number(s)
+        result_str = separator.join(
+            self.generate_random_number(digits, lower_num, upper_num) for _ in range(num_count))
+
+        yield self.create_text_message(result_str)
+
+    @staticmethod
+    def generate_random_number(digits: int, lower_num: float, upper_num: float) -> str:
+        if digits == 0:
             random_num = random.randint(int(lower_num), int(upper_num))
         else:
             random_num = round(random.uniform(lower_num, upper_num), digits)
 
-        yield self.create_text_message(str(random_num))
+        return str(random_num)
